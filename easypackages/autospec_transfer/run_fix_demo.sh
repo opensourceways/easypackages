55c415781e918de987ed6b302a988c073426e316
liulxb <1964023718@qq.com>
2024-10-30 09:06:21 +0800

    【优化】fedora40的映射关系、autospec部分优化代码

--- easypackages/autospec_transfer/run_fix_demo.sh
+++ easypackages/autospec_transfer/run_fix_demo.sh
@@ -1,5 +1,49 @@
 #!/bin/bash
 
+wget_srpm()
+{
+    add_user_mock
+    # shellcheck disable=SC2154
+    local src_rpm_name=${repo_addr//wget:/}
+    echo "[log] src rpm from wget: ${src_rpm_name}  ${repo_addr}"
+    wget -P /tmp/ https://api.compass-ci.openeuler.org:20007/result/rpmbuild-test-lxb/tmp/"${src_rpm_name}"
+    ls /tmp/
+    echo "[log] src rpm from wget succ."
+
+    # 此处超长，记得取消换行 --开始
+    su lkp -c "rpm -i --noplugins --nosignature /tmp/${src_rpm_name} >/dev/null" || \
+    su lkp -c "rpm -i --noplugins --nosignature /tmp/${src_rpm_name}>/dev/null" || \
+    die "failed to install source rpm: ${repo_addr}"
+    # --结束
+
+    get_srpm_readme
+}
+
+#原有接口，增加了从z9服务器wget场景
+from_srpm()
+{
+    [ -n "$repo_addr" ] || die "repo_addr is empty"
+
+    if [[ "${repo_addr##*.}" = "rpm" && "${repo_addr}" =~ ^http ]]; then
+        # 源码仓获取
+        install_srpm
+        rpmbuild_type="srpm"
+    elif [[ "${repo_addr}" =~ ^wget: ]]; then
+        # 从z9获取
+        wget_srpm
+    elif [[ "${repo_addr##*.}" = "git" ]]; then
+        # git仓库获取
+        clone_srpm
+    else
+        #pyp2rpm_spec
+        pyporter_spec
+        # shellcheck disable=SC2034
+        rpmbuild_type="pyporter_spec"
+        #rpmbuild_type="pyp2rpm_spec"
+    fi
+}
+
+
 #------------------------------------------------------------------
 #   说明：
 #       在autospec迁移过程中，用于解决%files阶段路径错误问题
@@ -67,7 +111,7 @@ autospec_build_rpm()
 
             su - lkp -c "rpmbuild -ba ${spec_dir}/*.spec ${res_rpm_file_exten} 2>&1 | tee ${rpmbuild_result_path}/build.log;exit \${PIPESTATUS[0]}"
             if [ "${PIPESTATUS[0]}" -ne 0 ]; then
-	            repair_require || root_attempt
+                repair_require || root_attempt
                 # shellcheck disable=SC2181
                 [ $? -ne 0 ] && return 1
             fi

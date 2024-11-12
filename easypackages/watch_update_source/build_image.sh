931d5eb4bef28050990b4da465d478bcb11d989e
jlcoo <longjianguestc@gmail.com>
2024-11-12 22:02:34 +0800

    fix build image bug

--- easypackages/watch_update_source/build_image.sh
+++ easypackages/watch_update_source/build_image.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-# /usr/bin/docker build --no-cache -t openeuler-pypi-package-update:latest .
-/usr/bin/docker build -t openeuler-pypi-package-update .
+# /usr/bin/docker build --no-cache -t openeuler-package-migration-update:latest .
+/usr/bin/docker build -t openeuler-package-migration-update .

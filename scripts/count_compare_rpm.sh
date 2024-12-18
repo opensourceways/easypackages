d3b781ee36989a814854381988ee11bb74e3e0d9
xiong-kq <1321766239@qq.com>
2024-12-18 17:01:34 +0800

    Update count_compare_rpm.sh

--- scripts/count_compare_rpm.sh
+++ scripts/count_compare_rpm.sh
@@ -1,4 +1,4 @@
-#!bin/bash
+#!/bin/bash
 # desc
 #根据不同场景，传入目录，统计出二进制rpm包，发布的二进制包，源码包，发布的源码包情况
 # create time: 20241216
@@ -11,7 +11,7 @@ PUB_SRC_RPM_PATH=$PUB_RPM_PATH
 
 
 check_params(){
-    if [ -z $RPM_PATH ] || [ -z $PUB_RPM_PATH ];then
+    if [ -z "$RPM_PATH" ] || [ -z "$PUB_RPM_PATH" ];then
         echo "[error] ONE OR MORE PARAM PATH ARE EMPTY!"
         echo "USAGE: sh count_compare_rpm.sh <构建成功RPM包路径> <发布成功RPM包路径>"
         exit 1
@@ -22,13 +22,13 @@ get_src_num(){
     # 输出包的信息，不带包版本
     local rpm_tmp_file=$1
     while read -r rpm_file; do
-        rpm -qp --queryformat "%{NAME}\n" ${rpm_file}
-    done < $rpm_tmp_file
+        rpm -qp --queryformat "%{NAME}\n" "${rpm_file}"
+    done < "$rpm_tmp_file"
 }
 
 lookup_count_rpm(){
     # 查找统计rpm包情况
-    system_type=`basename $RPM_PATH`  
+    system_type=$(basename $RPM_PATH)  
     rpm_info_tmp_file=$system_type"_rpm_1103.txt"
     rpm_pkg_tmp_file=$system_type"_rpm_package_name_1103.txt"
     rpm_uniq_tmp_file=$system_type"_uniq_rpm_1103.txt"
@@ -45,16 +45,16 @@ lookup_count_rpm(){
     src_pub_rpm_pkg_tmp_file=$system_type"_src_pub_rpm_package_name_1103.txt"
     src_pub_rpm_uniq_tmp_file=$system_type"_uniq_src_pub_rpm_1103.txt"
     # 二进制rpm包情况
-    find_process_rpm $rpm_info_tmp_file $rpm_pkg_tmp_file $rpm_uniq_tmp_file $RPM_PATH "N"
+    find_process_rpm "$rpm_info_tmp_file" "$rpm_pkg_tmp_file" "$rpm_uniq_tmp_file" $RPM_PATH "N"
     # 发布二进制包情况
-    find_process_rpm $pub_rpm_info_tmp_file $pub_rpm_pkg_tmp_file $pub_rpm_uniq_tmp_file $PUB_RPM_PATH "N"
+    find_process_rpm "$pub_rpm_info_tmp_file" "$pub_rpm_pkg_tmp_file" "$pub_rpm_uniq_tmp_file" "$PUB_RPM_PATH" "N"
     # 源码包情况
-    find_process_rpm $src_rpm_info_tmp_file $src_rpm_pkg_tmp_file $src_rpm_uniq_tmp_file $SRC_RPM_PATH "Y"
+    find_process_rpm "$src_rpm_info_tmp_file" "$src_rpm_pkg_tmp_file" "$src_rpm_uniq_tmp_file" "$SRC_RPM_PATH" "Y"
     # 发布源码包情况
-    find_process_rpm $src_pub_rpm_info_tmp_file $src_pub_rpm_pkg_tmp_file $src_pub_rpm_uniq_tmp_file $PUB_SRC_RPM_PATH "Y"
+    find_process_rpm "$src_pub_rpm_info_tmp_file" "$src_pub_rpm_pkg_tmp_file" "$src_pub_rpm_uniq_tmp_file" "$PUB_SRC_RPM_PATH" "Y"
 
     # 统计结果
-    count_result $system_type
+    count_result "$system_type"
 }
 
 find_process_rpm(){   
@@ -64,38 +64,38 @@ find_process_rpm(){
     local rpm_path=$4
     local src_flag=$5
     if [[ $src_flag == "N" ]];then
-        find $rpm_path -name "*rpm" | grep -v "src" > $rpm_info_tmp_file
+        find "$rpm_path" -name "*rpm" | grep -v "src" > "$rpm_info_tmp_file"
     else
-        find $rpm_path -name "*rpm" | grep "src" > $rpm_info_tmp_file
+        find "$rpm_path" -name "*rpm" | grep "src" > "$rpm_info_tmp_file"
     fi
-    get_src_num $rpm_info_tmp_file > $rpm_pkg_tmp_file
-    cat $rpm_pkg_tmp_file| sort|uniq -c|sort -nr|more > $rpm_uniq_tmp_file
+    get_src_num "$rpm_info_tmp_file" > "$rpm_pkg_tmp_file"
+    cat "$rpm_pkg_tmp_file"| sort|uniq -c|sort -nr|more > "$rpm_uniq_tmp_file"
 }
 
 count_result(){
     # 统计结果
     system_type=$1
     # 未发布
-    build_success_total=`ls | grep "$system_type" | grep 1103 | grep -v pub |grep -v name | xargs wc -l`
+    build_success_total=$(ls | grep "$system_type" | grep 1103 | grep -v pub |grep -v name | xargs wc -l)
 
     # 发布
-    pub_success_total=`ls | grep "$system_type" | grep 1103 | grep pub |grep -v name | xargs wc -l`
+    pub_success_total=$(ls | grep "$system_type" | grep 1103 | grep pub |grep -v name | xargs wc -l)
     # 源码包构建成功个数
-    src_rpm_build_total=`wc -l "$src_rpm_info_tmp_file"|awk -F ' ' '{print $1}'`
+    src_rpm_build_total=$(wc -l "$src_rpm_info_tmp_file"|awk -F ' ' '{print $1}')
     # 源码包构建成功包名称个数
-    src_rpm_build_package_total=`wc -l "$src_rpm_uniq_tmp_file"|awk -F ' ' '{print \$1}'`
+    src_rpm_build_package_total=$(wc -l "$src_rpm_uniq_tmp_file"|awk -F ' ' '{print \$1}')
     # 二进制包构建成功个数
-    rpm_build_total=`wc -l "$rpm_info_tmp_file"|awk -F ' ' '{print \$1}'`
+    rpm_build_total=$(wc -l "$rpm_info_tmp_file"|awk -F ' ' '{print \$1}')
     # 二进制包构建成功包名称个数
-    rpm_build_package_total=`wc -l "$rpm_uniq_tmp_file"|awk -F ' ' '{print \$1}'`
+    rpm_build_package_total=$(wc -l "$rpm_uniq_tmp_file"|awk -F ' ' '{print \$1}')
     # 发布成功源码包个数
-    pub_src_rpm_total=`wc -l "$src_pub_rpm_info_tmp_file"|awk -F ' ' '{print \$1}'`
+    pub_src_rpm_total=$(wc -l "$src_pub_rpm_info_tmp_file"|awk -F ' ' '{print \$1}')
     # 发布成功源码包名称个数
-    pub_src_rpm_package_total=`wc -l "$src_pub_rpm_uniq_tmp_file"|awk -F ' ' '{print \$1}'`
+    pub_src_rpm_package_total=$(wc -l "$src_pub_rpm_uniq_tmp_file"|awk -F ' ' '{print \$1}')
     # 发布成功二进制包个数
-    pub_rpm_build_total=`wc -l "$pub_rpm_info_tmp_file"|awk -F ' ' '{print \$1}'`
+    pub_rpm_build_total=$(wc -l "$pub_rpm_info_tmp_file"|awk -F ' ' '{print \$1}')
     # 发布成功二进制包名称个数
-    pub_rpm_build_package_total=`wc -l "$pub_rpm_uniq_tmp_file"|awk -F ' ' '{print \$1}'`
+    pub_rpm_build_package_total=$(wc -l "$pub_rpm_uniq_tmp_file"|awk -F ' ' '{print \$1}')
 
     echo "no_pub_total=$build_success_total,pub_total=$pub_success_total"
 
